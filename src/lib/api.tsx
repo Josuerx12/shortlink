4224a76990be04ad282dde107fd0d9e29f54182e
josuerx12 <josueaze12@gmail.com>
2025-10-31 20:02:28 -0300

    fix: set default baseURL for API if VITE_API_URL is not defined

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL ?? "https://api-jcshortlink.jcdev.com.br/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
